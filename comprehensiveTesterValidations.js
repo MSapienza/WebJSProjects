d246f7ec3c3b6f400fe7747761115e406b89056f
MJS <msapienza.28@gmail.com>
2019-11-05 14:22:32 -0600

    Building skeleton further

--- comprehensiveTesterValidations.js
+++ comprehensiveTesterValidations.js
@@ -7,3 +7,33 @@ function validatePhone(text3)
      document.getElementById("text3").innerHTML = text3;
 
 }
+
+function returnTextSleepType(value)
+{
+     var message = "";
+
+     if (value == "Deep Sleeper")
+     {
+          message = "You must be tired!";
+     }
+     else if (value == "Light Sleeper")
+     {
+          message = "You must be cautious!";
+     }
+     else if (value == "Toss and Turning")
+     {
+          message = "You must be restless!";
+     }
+     else if (value == "Sleeptalker")
+     {
+          message = "Your brain never turns off!";
+     }
+     document.getElementById("result").innerHTML = message;
+     return message;
+
+}
+
+function popupPersonality(this.value)
+{
+
+}
