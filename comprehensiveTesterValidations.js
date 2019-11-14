e8adb403777914912c05e0074b53b4f071b0ed2d
MJS <msapienza.28@gmail.com>
2019-11-14 00:49:07 -0600

    Updated some functionality

--- comprehensiveTesterValidations.js
+++ comprehensiveTesterValidations.js
@@ -1,8 +1,24 @@
 
-function validatePhone(text3)
+function validateTestInput(text3)
 {
-     var validationMessage = "";
-     document.getElementById("text3").innerHTML = text3;
+     var phone = "", message = "";
+     phone = document.getElementById("phone").value;
+
+     if (phone.length > 10 || phone.length < 10)
+     {
+          message = "Phone must be 10 digits!";
+     }
+     else if (phone.match(/[a-z]/i))
+     {
+          message = "Phone numbers must be numeric!";
+     }
+     else
+     {
+          message = "Validated!";
+     }
+
+     document.getElementById("result").innerHTML = message;
+     return message;
 }
 
 function returnTextSleepType(value)
@@ -25,11 +41,6 @@ function returnTextSleepType(value)
      {
           message = "Your brain never turns off!";
      }
-     document.getElementById("result").innerHTML = message;
+     document.getElementById("result2").innerHTML = message;
      return message;
-}
-
-function popupPersonality(value)
-{
-
 }
