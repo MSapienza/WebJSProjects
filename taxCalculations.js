0dacdf91c3400ce9970e1ca11d6caaea35fe2318
MJS <msapienza.28@gmail.com>
2019-11-19 17:17:03 -0600

    Updates on Tax Bracket UI

--- taxCalculations.js
+++ taxCalculations.js
@@ -14,10 +14,25 @@ function databaseHandler()
 
 function taxBracketCalculator(salary)
 {
+     //tax is calculated based on the tax bracket the user wants
+     //tax bracket can be Missouri, or fictional
 
 }
 
 function setTaxBracketInfo(NewTaxBracket)
 {
+     //user submits new Tax Bracket linked to fictional state
 
 }
+
+
+//tax data in db 
+//table name should be tax_bracket_t;
+//columns: tax_state varchar (200), taxable_salary decimal, taxable_rate, updt_user_id, updt_dtm
+
+// con.connect(function(err) {
+//      if (err) throw err;
+//      con.query("SELECT * FROM customers", function (err, result, fields) {
+//        if (err) throw err;
+//        console.log(result);
+//      });
