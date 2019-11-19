edc850134598d0d0be5bb398ef4c183e5b5ea33a
MJS <msapienza.28@gmail.com>
2019-11-19 17:58:38 -0600

    Minor tweaks to Tax Bracket UI

--- taxCalculations.js
+++ taxCalculations.js
@@ -12,17 +12,15 @@ function databaseHandler()
      });
 }
 
-function taxBracketCalculator(salary)
+function taxBracketCalculator(salary, bracket)
 {
      //tax is calculated based on the tax bracket the user wants
-     //tax bracket can be Missouri, or fictional
-
+     //tax bracket can be Missouri, or fictional  
 }
 
 function setTaxBracketInfo(NewTaxBracket)
 {
      //user submits new Tax Bracket linked to fictional state
-
 }
 
 // con.connect(function(err) {
