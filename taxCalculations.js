dc3cbc106ad80d1fc825b8331c8b7909faf9e6c6
MJS <msapienza.28@gmail.com>
2019-11-19 22:03:18 -0600

    Trying to figure out Node.js....

--- taxCalculations.js
+++ taxCalculations.js
@@ -1,16 +1,11 @@
 
-function databaseHandler()
-{
-     var mysql = require('mysql');
+var mysql = require('mysql');
+var connection = mysql.createConnection({host: 'localhost', user: 'jsuser', password: 'jsuser', database: 'sys'});
+connection.connect(function(err) {
+     if (err) console.log(err);
+ });
 
-     var con = mysql.createConnection({host: "localhost", user: "jsuser", password: "jsuser"});
-
-     con.connect(function(err) 
-     {
-          if (err) throw err;
-          console.log("Connected!");
-     });
-}
+ connection.end(); 
 
 function taxBracketCalculator(salary, bracket)
 {
@@ -23,9 +18,40 @@ function setTaxBracketInfo(NewTaxBracket)
      //user submits new Tax Bracket linked to fictional state
 }
 
-// con.connect(function(err) {
-//      if (err) throw err;
-//      con.query("SELECT * FROM customers", function (err, result, fields) {
-//        if (err) throw err;
-//        console.log(result);
-//      });
+function viewTaxBracket()
+{
+     var brackets = document.getElementById("bracket").value;
+     var salaries = [], taxRates = [], message;
+     
+      
+     
+     connection.query('SELECT * FROM book_collection_t',
+          function (err, result) {
+            if (err) console.log(err);
+            console.log(result);
+          });
+
+          document.getElementById("bracketResult").innerHTML = "test";    
+     
+     
+     if (brackets == "MO")
+     {
+          //pull MO bracket
+      
+     }
+     else if (brackets == "Federal")
+     {
+          //pull federal bracket
+
+     }
+     else if (brackets == "Custom")
+     {
+          //pull custom bracket or throw warning if none exists
+
+     }
+
+     document.getElementById("bracketResult").innerHTML = "mattTest";
+     return message;
+
+     //figure out how to return full table into bracketResults
+}
